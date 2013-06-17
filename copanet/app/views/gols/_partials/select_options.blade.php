f67dd0dc3e52ade9476cfd4f0fe4a61255a39700
Davi Alves <flyingkrai@hotmail.com>
2013-06-17 15:42:51 -0300

    cleaning

--- copanet/app/views/gols/_partials/select_options.blade.php
+++
@@ -1,7 +0,0 @@
-<option value="">Selecione</option>
-@foreach($entities as $entity)
-  <option value="{{ $entity->id }}"
-    @if(isset($route)) data-url="{{ route($route, $entity->id) }}" @endif>
-    {{ $entity->nome }}
-  </option>
-@endforeach
